27c03e223ab04a963cfff064663d1d3cc5429257
simfeo <simfeo@yandex.ru>
2024-04-18 17:12:43 +0100

    implemented axis locks. Version bump to 1.0.0. Version 0.2.0 is published

--- vertex_to_arch.py
+++ vertex_to_arch.py
@@ -1,13 +1,15 @@
 import bpy
+import mathutils
 from bpy.types import Operator
 from bpy.props import (
         FloatProperty,
+        EnumProperty,
 )
 
 bl_info = {
     "name": "Make arch",
     "author": "idimus (Smbat Makiyan)",
-    "version": (0, 1, 0),
+    "version": (1, 0, 0),
     "blender": (4, 0, 0),
     "location": "View3D > Sidebar > Edit Tab / Edit Mode Context Menu/ Make arch",
     "warning": "",
@@ -46,6 +48,18 @@ class MeshToolsMakeArch(Operator):
         soft_min=-1024.0,
         soft_max=1024.0
         )
+    
+    lock_axis: EnumProperty(
+        name="LockAxis",
+        items=(("none", "None", "Do not lock axis"),
+               ("screen", "Screen", "Lock by current screen rotation"),
+               ("x", "X", "Lock X"),
+               ("y", "Y", "Lock Y"),
+               ("z", "Z", "Lock Z"),
+              ),
+        description="Lock axis for radius",
+        default='none'
+        )
 
     @classmethod
     def poll(cls, context):
@@ -67,6 +81,54 @@ class MeshToolsMakeArch(Operator):
     def set_new_position(self, vertex, position):
         vertex.co = position
 
+    def make_arc_simple(self, selectedVerts):
+        if self.radius == 0.0 or self.radius == 0:
+            length = map(lambda x: self.calc_distance(x), selectedVerts)
+            sum_length = sum(length)
+            self.radius = sum_length/len(selectedVerts)
+
+        for v in  selectedVerts:
+            self.set_new_position(v, self.calc_new_position(v, self.radius))
+    
+    def calc_distance_normal(self, vertex, normal):
+        # proj p = P*Q/Q.length x Q
+        # where Q is normal and unit vector, so Q.lenght == 1
+        proj = normal * (vertex.co@normal)
+        return proj.length
+    
+    def calc_new_position_normal(self, vertex, length, normal):
+        vec_normalized = vertex.co
+        proj = normal * (vertex.co@normal)
+        vec_normalized = vec_normalized - proj
+        vec_normalized.normalize()
+        vec_normalized = vec_normalized * length
+        return vec_normalized + proj, proj
+    
+    # normal is normal vector for plane
+    def make_arc_normal(self, selectedVerts, normal):
+        if self.radius == 0.0 or self.radius == 0:
+            length = map(lambda x: self.calc_distance_normal(x, normal), selectedVerts)
+            sum_length = sum(length)
+            self.radius = sum_length/len(selectedVerts)
+            
+        nn = []
+        for v in  selectedVerts:
+            pos, proj = self.calc_new_position_normal(v, self.radius, normal)
+            nn.append(proj)
+            self.set_new_position(v, pos)
+        
+        # mm = MeshToolsMakeArch.create_pointed_mesh("mmmm", nn)
+        # bpy.context.collection.objects.link(mm)
+            
+            
+    # def create_pointed_mesh(ob_name, coords, edges=[], faces=[]):
+    #     me = bpy.data.meshes.new(ob_name + "Mesh")
+    #     ob = bpy.data.objects.new(ob_name, me)
+    #     me.from_pydata(coords, edges, faces)
+    #     ob.show_name = True
+    #     me.update()
+    #     return ob
+
     @in_object_mode
     def make_arc(self):
         selectedVerts = [v for v in bpy.context.active_object.data.vertices if v.select]
@@ -75,13 +137,32 @@ class MeshToolsMakeArch(Operator):
             self.report({'WARNING'}, "too few points, should be at least 2")
             return {'CANCELLED'}
         
-        if self.radius == 0.0 or self.radius == 0:
-            length = map(lambda x: self.calc_distance(x), selectedVerts)
-            sum_length = sum(length)
-            self.radius = sum_length/len(selectedVerts)
+        if self.lock_axis == "none":
+            self.make_arc_simple(selectedVerts)
+        else:
+            # 1,0,0
+            # <Vector (0.0000, 1.0000, 0.0000)> <Quaternion (w=0.5000, x=0.5000, y=0.5000, z=0.5000)> x
+            # <Vector (-1.0000, 0.0000, -0.0000)> <Quaternion (w=0.0000, x=-0.0000, y=0.7071, z=0.7071)> y 
+            # <Vector (1.0000, 0.0000, 0.0000)> <Quaternion (w=1.0000, x=-0.0000, y=-0.0000, z=-0.0000)> z
+            # 0,0,1
+            # <Vector (1.0000, 0.0000, 0.0000)> <Quaternion (w=0.5000, x=0.5000, y=0.5000, z=0.5000)> x
+            # <Vector (0.0000, 1.0000, 0.0000)> <Quaternion (w=0.0000, x=-0.0000, y=0.7071, z=0.7071)> y
+            # <Vector (0.0000, 0.0000, 1.0000)> <Quaternion (w=1.0000, x=-0.0000, y=-0.0000, z=-0.0000)> z
+            vec = mathutils.Vector((0,0,1))
+            if self.lock_axis == "screen":
+                view_quat = bpy.context.region_data.view_rotation
+                vec = mathutils.Vector((0,0,1))
+                vec = view_quat@vec
+                # print(view_quat@vec, view_quat)
+            elif self.lock_axis == "x":
+                vec = mathutils.Vector((1,0,0))
+            elif self.lock_axis == "y":
+                vec = mathutils.Vector((0,1,0))
+            elif self.lock_axis == "z":
+                vec = mathutils.Vector((0,0,1))
 
-        for v in  selectedVerts:
-            self.set_new_position(v, self.calc_new_position(v, self.radius))
+            self.make_arc_normal(selectedVerts, vec)
+
 
         return{'FINISHED'}
 
